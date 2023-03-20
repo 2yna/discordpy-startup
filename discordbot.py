71168315e4ad2721a47738fa4d0ae50e0b11b3ce
2yna <128389069+2yna@users.noreply.github.com>
2023-03-20 23:57:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,7 +19,28 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+ 
+client.on('messegeCreate',message => {
+  if (message.author.bot) return;
+    if (message.guild.id !== "1086670878084956300")
+      if (message.cotent.match(/[0-9][0-9][0-9][0-9][0-9]/))
+        if(message.cotent.match(/[^0-9]/)) return;
+        if(message.content.match(/[0-9][0-9][0-9][0-9][0-9][0-9]/)) return;
+     client.channels.cache.get('1086678028396003328').send(`${message.content}`)
+   }
+});
 
+
+
+client.on('messageCreate' ,message => {
+  if(message.guild.id !== "1086678028396003328")
+  return;
+    if(message.channel.id !=== '1086644609016397848'){
+  message.channel.setName("vc〖" + `${message.content}` + "〗")
+  return;
+  }
+});
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
